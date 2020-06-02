fa1047297068d35bbe6e348341b37ce92d795731
Prima Wirawan <primailkom@gmail.com>
2020-06-02 22:37:28 +0700

    Map todos component element into page

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -2,11 +2,8 @@ import React, { Component } from 'react'
 
 export default class Todos extends Component {
     render() {
-        console.log(this.props.todos)
-        return (
-            <div>
-                <h1>Hello from Todos</h1>
-            </div>
-        )
+        return this.props.todos.map((todo) => (
+            <h3>{ todo.title }</h3>
+        ));
     }
 }
